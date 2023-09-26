1fefd67972fa12462058b9a5284180968893982b
muhammadamin <mubashshirahmad999@gmail.com>
2023-09-26 18:48:10 +0500

    hamma faylni gitga ulandi

---
+++ bin/najot_talim_uz.dart
@@ -0,0 +1,5 @@
+import 'package:najot_talim_uz/najot_talim_uz.dart' as najot_talim_uz;
+
+void main(List<String> arguments) {
+  print('Hello world: ${najot_talim_uz.calculate()}!');
+}
---
+++ test/najot_talim_uz_test.dart
@@ -0,0 +1,8 @@
+import 'package:najot_talim_uz/najot_talim_uz.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
